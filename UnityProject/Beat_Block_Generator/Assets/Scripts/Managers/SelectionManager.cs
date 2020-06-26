34d48ecc8490ee559ff8869d9b6f59757b6ea418
MrJohnWeez <johnwakeboarding@gmail.com>
2020-06-26 17:41:55 -0400

    Redesigned layout, made custom assets, started working on dynamic lists

---
+++ UnityProject/Beat_Block_Generator/Assets/Scripts/Managers/SelectionManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Animator))]
+public class SelectionManager : MonoBehaviour
+{
+	private Animator _dragAndDrop = null;
+
+	private void Start()
+	{
+		_dragAndDrop = GetComponent<Animator>();
+	}
+
+	public void PointerEnter()
+	{
+		_dragAndDrop.SetBool("PointerEntered", true);
+	}
+
+	public void PointerExit()
+	{
+		_dragAndDrop.SetBool("PointerEntered", false);
+	}
+
+	public void PointerClicked()
+	{
+		Debug.Log("Clicked");
+	}
+}
---
+++ UnityProject/Beat_Block_Generator/Assets/Scripts/ObjectScrips/UI/MouseOverColorChange.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectionHover : MonoBehaviour
+{
+	[SerializeField] private GameObject _icons = null;
+	[SerializeField] private Image _background = null;
+
+	public void PointerEnter()
+	{
+		Debug.Log("Enter");
+	}
+
+	public void PointerExit()
+	{
+		Debug.Log("Exit");
+	}
+}
