2e8e9109b1b36ae3470638a41749e5395b6d71a6
MrJohnWeez <johnwakeboarding@gmail.com>
2020-07-05 20:36:51 -0400

    Improved unity app UI

--- UnityProject/Beat_Block_Generator/Assets/Scripts/Managers/WebManager.cs
+++ UnityProject/Beat_Block_Generator/Assets/Scripts/Managers/WebManager.cs
@@ -15,6 +15,11 @@ public class WebManager: MonoBehaviour
 	/// </summary>
     public void ToMrJohnWeezHelpSite()
 	{
+		Application.OpenURL("https://www.mrjohnweez.com/blocksaber.html");
+	}
+
+	public void ToMrJohnWeezSite()
+	{
 		Application.OpenURL("https://www.mrjohnweez.com/");
 	}
 }
